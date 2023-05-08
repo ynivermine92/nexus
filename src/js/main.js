230e311dfc6740d5d1ad4897d18272f6cedf6752
unknown <ynivermine92@gmail.com>
2023-05-08 13:43:45 +0300

    add searsh

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,23 @@
-// Мобильное меню бургер
-/* function burgerMenu() {
-    const burger = document.querySelector('.burger')
-    const menu = document.querySelector('.menu')
-    const body = document.querySelector('body')
-    burger.addEventListener('click', () => {
-        if (!menu.classList.contains('active')) {
-            menu.classList.add('active')
-            burger.classList.add('active')
-            body.classList.add('locked')
-        } else {
-            menu.classList.remove('active')
-            burger.classList.remove('active')
-            body.classList.remove('locked')
-        }
-    })
-    // Вот тут мы ставим брейкпоинт навбара
-    window.addEventListener('resize', () => {
-        if (window.innerWidth > 991.98) {
-            menu.classList.remove('active')
-            burger.classList.remove('active')
-            body.classList.remove('locked')
-        }
-    })
-}
-burgerMenu() */
 
-function tabs(headerSelector, tabSelector, contentSelector, activeClass, display='flex') {
+
+
+let search = document.querySelector('.search');
+let tabsInput = document.querySelector('.tabs__input');
+
+search.addEventListener('click',() =>{
+
+    search.classList.toggle('active')
+    if(search.classList.contains('active')){
+        tabsInput.classList.remove('none')
+        tabsInput.classList.add('block')
+    }else{
+        tabsInput.classList.remove('block')
+        tabsInput.classList.add('none')
+    }
+})
+
+
+ function tabs(headerSelector, tabSelector, contentSelector, activeClass, display='flex') {
     const header = document.querySelector(headerSelector),
           tab = document.querySelectorAll(tabSelector),
           content = document.querySelectorAll(contentSelector)
@@ -55,10 +47,38 @@ function tabs(headerSelector, tabSelector, contentSelector, activeClass, display
             });
         }
     })
-}
+} 
 
 // ПЕРВЫЙ аргумент - класс всего нашего хедера табов.
 // ВТОРОЙ аргумент - класс конкретного элемента, при клике на который будет переключатся таб.
 // ТРЕТИЙ аргумент - класс того блока, который будет переключаться.
 // ЧЕТВЕРТЫЙ аргумент - класс активности, который будет добавлятся для таба, который сейчас активен.
 tabs( '.tabs__header' ,'.tabs__header-item', '.tabs__content-item', 'active')
+
+
+// Мобильное меню бургер
+function burgerMenu() {
+    const burger = document.querySelector('.burger')
+    const menu = document.querySelector('.menu')
+    const body = document.querySelector('body')
+    burger.addEventListener('click', () => {
+        if (!menu.classList.contains('active')) {
+            menu.classList.add('active')
+            burger.classList.add('active')
+            body.classList.add('locked')
+        } else {
+            menu.classList.remove('active')
+            burger.classList.remove('active')
+            body.classList.remove('locked')
+        }
+    })
+    // Вот тут мы ставим брейкпоинт навбара
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 991.98) {
+            menu.classList.remove('active')
+            burger.classList.remove('active')
+            body.classList.remove('locked')
+        }
+    })
+}
+burgerMenu() 
