a5595f8c3169ae6e4e445422578c16f628cad859
unknown <ynivermine92@gmail.com>
2023-05-19 17:39:06 +0300

    test

---
+++ src/js/main.js
@@ -0,0 +1,84 @@
+
+
+
+let search = document.querySelector('.search');
+let tabsInput = document.querySelector('.tabs__input');
+
+search.addEventListener('click',() =>{
+
+    search.classList.toggle('active')
+    if(search.classList.contains('active')){
+        tabsInput.classList.remove('none')
+        tabsInput.classList.add('block')
+    }else{
+        tabsInput.classList.remove('block')
+        tabsInput.classList.add('none')
+    }
+})
+
+
+ function tabs(headerSelector, tabSelector, contentSelector, activeClass, display='flex') {
+    const header = document.querySelector(headerSelector),
+          tab = document.querySelectorAll(tabSelector),
+          content = document.querySelectorAll(contentSelector)
+    function hideTabContent() {
+        content.forEach(item => {
+            item.style.display = 'none'
+        });
+        tab.forEach(item => {
+            item.classList.remove(activeClass)
+        });
+    }
+    function showTabContent(i = 0) {
+       content[i].style.display = display
+       tab[i].classList.add(activeClass)
+    }
+    hideTabContent()
+    showTabContent()
+    header.addEventListener('click', e => {
+        const target = e.target
+        if (target.classList.contains(tabSelector.replace(/\./, '')) || 
+        target.parentNode.classList.contains(tabSelector.replace(/\./, ''))) {
+            tab.forEach((item, i) => {
+                if ( target == item || target.parentNode == item ) {
+                    hideTabContent()
+                    showTabContent(i)
+                }
+            });
+        }
+    })
+} 
+
+// ПЕРВЫЙ аргумент - класс всего нашего хедера табов.
+// ВТОРОЙ аргумент - класс конкретного элемента, при клике на который будет переключатся таб.
+// ТРЕТИЙ аргумент - класс того блока, который будет переключаться.
+// ЧЕТВЕРТЫЙ аргумент - класс активности, который будет добавлятся для таба, который сейчас активен.
+tabs( '.tabs__header' ,'.tabs__header-item', '.tabs__content-item', 'active')
+
+
+// Мобильное меню бургер
+function burgerMenu() {
+    const burger = document.querySelector('.burger')
+    const menu = document.querySelector('.menu')
+    const body = document.querySelector('body')
+    burger.addEventListener('click', () => {
+        if (!menu.classList.contains('active')) {
+            menu.classList.add('active')
+            burger.classList.add('active')
+            body.classList.add('locked')
+        } else {
+            menu.classList.remove('active')
+            burger.classList.remove('active')
+            body.classList.remove('locked')
+        }
+    })
+    // Вот тут мы ставим брейкпоинт навбара
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 991.98) {
+            menu.classList.remove('active')
+            burger.classList.remove('active')
+            body.classList.remove('locked')
+        }
+    })
+}
+burgerMenu() 
